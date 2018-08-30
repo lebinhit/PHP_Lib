47fd3b110e26f7fe0838a0ff9358433e06f76332
johnny <johnny@forixwebdesign.com>
2018-08-30 11:54:45 +0700

    add file cybersource

---
+++ M2/cybersource_magento.php
@@ -0,0 +1,4 @@
+
+If you would like to test recurring billing please contact Customer Support at 1-800-709-7779 to enable it on your test account.  To enable this service in production please contact the CyberSource Sales department at 1-888-330-2300 or by email at sales@cybersource.com for further details.
+
+For more information about recurring billing, please refer to the documentation available at: https://www.cybersource.com/products/payment_processing/recurring_billing/
