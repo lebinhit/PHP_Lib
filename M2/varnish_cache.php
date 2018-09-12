df78d6959cade3fd78c7a3cd1ffba656fc412bbd
johnny <johnny@forixwebdesign.com>
2018-09-12 16:12:03 +0700

    varnish cache

--- M2/varnish_cache.php
+++ M2/varnish_cache.php
@@ -1,2 +1,13 @@
 http://www.networkinghowtos.com/howto/configure-varnish-to-allow-purging-the-cache/
 https://devdocs.magento.com/guides/v2.0/config-guide/varnish/use-varnish-cache.html
+
+Or add config in app/etc/env.php
+
+'http_cache_hosts' =>
+array (
+0 =>
+array (
+'host' => '172.18.80.10',
+'port' => '6081',
+)
+),
